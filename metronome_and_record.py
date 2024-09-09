641622b5125b56ef2dfba3391b4ba3bf2c18bc83
Darklaneanjana <darklaneanjana@gmail.com>
2024-09-09 18:34:45 +0530

    dont remember what I did

--- metronome_and_record.py
+++ metronome_and_record.py
@@ -1,26 +1,71 @@
 import sounddevice as sd
 from pydub import AudioSegment
 from pydub.generators import Sine
+import numpy as np
 import time
 import pyaudio
 import wave
 
 
 def play_metronome(interval, frequency, duration):
-    def generate_metronome_sound(interval, frequency, duration):
-        beep = Sine(frequency).to_audio_segment(duration=100)  # 100 ms beep
-        silence = AudioSegment.silent(
-            duration=int((interval - 0.1) * 1000)
-        )  # Silence for the rest of the interval
-        metronome = beep + silence
-        total_metronome = metronome * int(duration / interval)
-        return total_metronome
+    # def generate_metronome_sound(interval, frequency, duration):
+    #     beep = Sine(frequency).to_audio_segment(duration=2)  # 100 ms beep
+    #     silence = AudioSegment.silent(
+    #         duration=int((interval - 0.1) * 1000)
+    #     )  # Silence for the rest of the interval
+    #     metronome = beep + silence
+    #     total_metronome = metronome * int(duration / interval)
+    #     return total_metronome
 
-    metronome_sound = generate_metronome_sound(interval, frequency, duration)
-    metronome_sound.export("metronome.wav", format="wav")
+    # metronome_sound = generate_metronome_sound(interval, frequency, duration)
+    # metronome_sound.export("metronome.wav", format="wav")
 
-    # Start playback and recording
-    sd.play(metronome_sound.get_array_of_samples(), samplerate=sample_rate)
+    # # Start playback and recording
+    # sd.play(metronome_sound.get_array_of_samples(), samplerate=sample_rate)
+
+    p = pyaudio.PyAudio()
+
+    volume = 0.5  # range [0.0, 1.0]
+    fs = 44100  # sampling rate, Hz, must be integer
+    duration = .02  # in seconds, may be float
+    f = 1000.0  # sine frequency, Hz, may be float
+
+    # generate samples, note conversion to float32 array
+    samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+
+    # per @yahweh comment explicitly convert to bytes sequence
+    output_bytes = (volume * samples).tobytes()
+
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
+
+    # play. May repeat with different volume values (if done interactively)
+    start_time = time.time()
+    stream.write(output_bytes)
+    print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+
+
+
+    stream.stop_stream()
+
+    # save wav file
+    wf = wave.open("metronome.wav", 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paFloat32))
+    wf.setframerate(fs)
+    wf.writeframes(output_bytes)
+    wf.close()
+
+    stream.close()
+
+    p.terminate()
+
+
+
+
 
 
 def record_audio(format, channels, sample_rate, chunk, output_name):
@@ -59,10 +104,10 @@ def record_audio(format, channels, sample_rate, chunk, output_name):
 
 
 # Parameters
-duration = 0.2  # Duration of the recording in seconds
-interval = 0.2  # Interval of metronome (seconds)
-frequency_1 = 2000  # Frequency of the beep (Hz)
-frequency_2 = 4000  # Frequency of the beep (Hz)
+duration = 1  # Duration of the recording in seconds
+interval = 0.3  # Interval of metronome (seconds)
+frequency_1 = 900  # Frequency of the beep (Hz)
+frequency_2 = 1000  # Frequency of the beep (Hz)
 sample_rate = 44100  # Sample rate for recording and playback
 
 format = pyaudio.paInt16
@@ -76,5 +121,5 @@ play_metronome(interval, frequency_1, duration)
 record_audio(format, channels, sample_rate, chunk, output_name_1)
 
 
-play_metronome(interval, frequency_2, duration)
-record_audio(format, channels, sample_rate, chunk, output_name_2)
+# play_metronome(interval, frequency_2, duration)
+# record_audio(format, channels, sample_rate, chunk, output_name_2)
